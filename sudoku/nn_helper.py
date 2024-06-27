94af0194575dbbcd28835cf2c5f0e5404a11e7e9
agrero <thegrero@gmail.com>
2024-06-27 10:49:51 -0700

    removed batch_print

--- sudoku/nn_helper.py
+++ sudoku/nn_helper.py
@@ -2,7 +2,7 @@
 # become their own classes 
 # or a method in the dataloader
 
-def train(dataloader, model, loss_fn, optimizer, batch_print=100, device='cuda'):
+def train(dataloader, model, loss_fn, optimizer, device='cuda'):
 
     model.train()
 
@@ -11,11 +11,11 @@ def train(dataloader, model, loss_fn, optimizer, batch_print=100, device='cuda')
         X, y = X.to(device), y.to(device)
 
         pred = model(X)
-
         loss = loss_fn(pred, y) 
+
         loss.backward()
         optimizer.step()
 
-        if batch % batch_print == 0:
-            loss, current = loss.item(), (batch+1) * len(X)
-            print(f'loss: {loss:>7f} [{current:<5d}/{len(dataloader.dataset):>5d}]')
+        
+        loss, current = loss.item(), (batch+1) * len(X)
+        print(f'loss: {loss:>7f} [{current:<5d}/{len(dataloader.dataset):>5d}]')
