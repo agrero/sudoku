4886a69bbf016d5c2599595271caba8c0d1969a1
agrero <thegrero@gmail.com>
2024-07-29 19:09:00 -0700

    implemented general command structure

--- frontend/discord/DiscordClient.py
+++ frontend/discord/DiscordClient.py
@@ -1,12 +1,10 @@
 import discord
-import requests
+# import requests
 
-from fastapi.encoders import jsonable_encoder
+# from fastapi.encoders import jsonable_encoder
 
-from sudoku.pydantic.models import CommandIn
-from sudoku.helper.api_helper import get_command
-
-from sudoku.classes.game.Board import Board
+# from sudoku.pydantic.models import CommandIn
+# # from sudoku.helper.api_helper import get_command
 
 from frontend.discord.children.ClientHandler import ClientHandler
 
@@ -16,43 +14,23 @@ class CustomClient(discord.Client):
     Docstrings Here
     
     $h: Discord Bot Command Prompt
+
+    I think eventually this will swallow a more condensed
+    Client Handler class
     """
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
 
     # this should come from an inherited message handler object
+
     async def on_message(self, message, 
-                         url='http://0.0.0.0:8000', 
+                         url='http://0.0.0.0:8008', 
                          handler=ClientHandler()):
-
-        # DELETE ME 
-        board = Board()
-
+        
         if message.author == self.user:
             return
-        
-        if message.content.startswith('$h'):
-            await handler.post_solvehandler(message, url)
 
-
-        # this will need to utilize the api but do what it says below
         if message.content.startswith('$s'):
-            await handler.get_boardstate(message, url)
-            
 
-            if message.content[0] == 'set':
-                # send to api
-                board.set_tile(
-                    message.content[3], # number
-                    message.content[2], # row
-                    message.content[1]  # column
-                )
-                # handle api response (should save in db with each move)
-                await message.channel.send(board.board)
-            
-            if message.content[0] == 'load':
-                # send to api
-                # # lookup user name in db
-                # # return last saved game
-                # handle response
-                pass
+            await handler.get_boardstate(message, url)
+
--- frontend/discord/children/ClientHandler.py
+++ frontend/discord/children/ClientHandler.py
@@ -1,33 +1,28 @@
 from sudoku.helper.api_helper import get_command
 import requests
 
-from sudoku.pydantic.models import CommandIn
+from sudoku.schemas.api import CommandIn
 from fastapi.encoders import jsonable_encoder
 
 from sudoku.classes.game.Board import Board
 
+
+# MANIUPLATIONS SHOULD HAPPEN NOT HERE
+# ALSO TRY AND SLIM THIS DOWN AND MAKE IT PART OF THE CUSTOMCLIENT
 class ClientHandler:
 
-    async def post_solvehandler(self, message, url):
-        commands = get_command(message.content[2:])
-        post = requests.post(
-            url=f'{url}/{commands[0]}',
-            headers={'Content-Type':'application/json'},
-            json=jsonable_encoder(
-                CommandIn(
-                    commands=commands,
-                    user=message.author.name,
-                    message_id=message.id,
-                    guild_id=message.guild.id
-            )))
-        
-        board = Board()
-        board.board = post.json()['solved_board']
-        response = board.pretty_rep()
+    async def get_boardstate(self, message, url:str) -> requests.Response:
+        """
+        Unpack Commands and send 
+        a request to the database API
 
-        await message.channel.send(response)
+        message: a message in the form of an on_message discord 
+            response
+        url: string url to send 
 
-    async def get_boardstate(self, message, url):
+        returns: requests.Response
+        """
+
         commands = get_command(message.content[2:])
         post = requests.post(
             url=f'{url}/{'/'.join(commands)}', #URL should unwrap all commands
@@ -40,4 +35,13 @@ class ClientHandler:
                     guild_id=message.guild.id
             )))
         
-        await message.channel.send(post.json())
+        return post
+
+    async def send_board(self, message, url:str):
+        
+        # post to get boardstate 
+        post = self.get_boardstate(message=message, url=url)
+        await message.channel.send(Board(
+            board=post.json()['solved_board']
+        ).pretty_rep())
+
