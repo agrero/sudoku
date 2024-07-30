9da23357894c44b458dc3bcddbf79f4b839d6d23
agrero <thegrero@gmail.com>
2024-07-29 17:07:00 -0700

    renamed dir

--- sudoku/schemas/sudoku_schemas.py
+++ sudoku/schemas/sudoku_schemas.py
@@ -1,19 +1,6 @@
 from pydantic import BaseModel
-from sudoku.pydantic.forms.examples import *
-
-
-class CommandIn(BaseModel):
-    # theoretically we could make our own allowed string class
-    # to make commands: list[AllowedStr]
-    commands: list[str] 
-    user: str
-    message_id: int
-    guild_id: int
-
-class CommandOut(BaseModel):
-    com_return: dict
-    com_in: CommandIn | None = None # return the commands input
-
+from sudoku.schemas.forms.examples import *
+from ..schemas.api_schemas import CommandIn
 
 class SudokuIn(BaseModel):
     """Sudoku Input Model
